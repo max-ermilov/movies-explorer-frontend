7bcbd58d266b81022498d0afce9261512de3c700
max-ermilov <max-ermilov@yandex.ru>
2022-09-07 13:46:28 +0300

    fix initial cards

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 import './MoviesCardList.css';
 import MoviesCard from "../MoviesCard/MoviesCard";
-import cardsList from "../../utils/initialEmptyMoviesCardsList";
+// uncomment to hide cards
+// import cardsList from "../../utils/initialEmptyMoviesCardsList";
+
+// comment to hide cards
+import cardsList from "../../utils/initialMoviesCardsList";
 
 function MoviesCardList() {
   return (
